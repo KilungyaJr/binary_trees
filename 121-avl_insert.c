fe08673639ebaea4489a984ca7a39806abacdf86
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-01-21 09:27:06 +0300

    fixed some code fails

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -7,26 +7,6 @@ avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 avl_t *avl_insert(avl_t **tree, int value);
 
 /**
- * binary_tree_height - this measures the height of a binary tree
- * @tree:  pointer to the root node of the tree to measure the height
- * Return: If tree is NULL, your function must return 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-        size_t left = 0;
-        size_t right = 0;
-
-        if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-                return (0);
-
-        left = binary_tree_height(tree->left);
-        right = binary_tree_height(tree->right);
-
-        return ((left > right ? left : right) + 1);
-}
-
-
-/**
  * height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
  *
